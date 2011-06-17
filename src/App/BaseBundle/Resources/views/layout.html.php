9442f24e7401fe39977de1af6d7f46a227324fa2
lidaa <aa_dil@hotmail.fr>
2011-06-17 17:00:01 +0100

    add login an logout (UserBundle)

--- src/App/BaseBundle/Resources/views/layout.html.php
+++ src/App/BaseBundle/Resources/views/layout.html.php
@@ -8,22 +8,33 @@
 	<link rel="stylesheet" href="<?php echo $view['assets']->getUrl('css/main.css') ?>" type="text/css" media="all" />
 </head>
 <body>
-	<div class="container">
-		<div class="top">
-			<a href="<?php echo $view['router']->generate('_home_index') ?>"><span>Lidaa blog</span></a>
-		</div>
-		<div class="header"></div>
-		<div class="clearer"><span></span></div>
-		<div class="main">
-			<div class="content">
-				<?php $view['slots']->output('content') ?>
-			</div>
-			<div class="bar">
-				<?php echo $view->render('AppBaseBundle::sidebar.html.php') ?>
-			</div>
-		</div>
-		<div class="clearer"><span></span></div>
-		<div class="footer">Site developped with <a href="http://www.symfony.com" target="blank">Symfony2</a></div>
-	</div>
+    <div class="container">
+        <div class="top">
+            <a href="<?php echo $view['router']->generate('_home_index') ?>"><span>Lidaa blog</span></a>
+            <?php if ($view['security']->isGranted('IS_AUTHENTICATED_REMEMBERED')): ?>
+                <a href="<?php echo $view['router']->generate('fos_user_security_logout') ?>">
+                    Déconnexion
+                    <?php $this->get('translator')->trans('layout.logout',array(),'FOSUserBundle'); ?>
+                </a>
+           <?php else: ?>
+               <a href="<?php echo $view['router']->generate('fos_user_security_login') ?>">
+                   Connexion
+                    <?php $this->get('translator')->trans('layout.login',array(),'FOSUserBundle'); ?>
+               </a>
+           <?php endif; ?>
+        </div>
+        <div class="header"></div>
+        <div class="clearer"><span></span></div>
+        <div class="main">
+            <div class="content">
+                    <?php $view['slots']->output('content') ?>
+            </div>
+            <div class="bar">
+                    <?php echo $view->render('AppBaseBundle::sidebar.html.php') ?>
+            </div>
+        </div>
+        <div class="clearer"><span></span></div>
+        <div class="footer">Site developped with <a href="http://www.symfony.com" target="blank">Symfony2</a></div>
+    </div>
 </body>
 </html>
