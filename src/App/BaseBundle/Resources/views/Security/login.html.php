3d58c45514e261d5a866561471d3c2f21ffb438d
lidaa <aa_dil@hotmail.fr>
2011-06-26 13:15:06 +0200

    style form login

--- src/App/BaseBundle/Resources/views/Security/login.html.php
+++ src/App/BaseBundle/Resources/views/Security/login.html.php
@@ -1,23 +1,28 @@
 <?php $view->extend('AppBaseBundle::layout.html.php'); ?>
 
 <?php $view['slots']->start('content') ?>
-	<div id="login-form">
+
+	<div class="msg_error">
 		<?php if ($error): ?>
 			<div><?php echo $error->getMessage() ?></div>
 		<?php endif; ?>
+	</div>
+
+	<div id="login-form">
 		<form action="<?php echo $view['router']->generate('login_check') ?>" method="post">
-			<label for="username">Username:</label>
-			<input type="text" id="username" name="_username" value="<?php echo $last_username ?>" />
-
-			<label for="password">Password:</label>
-			<input type="password" id="password" name="_password" />
-
+			<div class="form-row">
+				<label for="username">Username:</label>
+				<input type="text" id="username" name="_username" value="<?php echo $last_username ?>" />
+			</div>
+			<div class="form-row">
+				<label for="password">Password:</label>
+				<input type="password" id="password" name="_password" />
+			</div>
 			<!--
 				If you want to control the URL the user is redirected to on success (more details below)
 				<input type="hidden" name="_target_path" value="/account" />
 			-->
-
-			<input type="submit" name="login" />
+			<input type="submit" name="login" id="form-submit" />
 		</form>
 	</div>
 <?php $view['slots']->stop(); ?>
