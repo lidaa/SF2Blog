f919c70cca7a887e529ca920ba7bd04a11bebe87
lidaa <aa_dil@hotmail.fr>
2011-07-03 21:59:51 +0200

    BaseController

--- src/App/BaseBundle/Controller/BaseController.php
+++ src/App/BaseBundle/Controller/BaseController.php
@@ -8,10 +8,54 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller,
 
 class BaseController extends Controller
 {
+
 	/**
 	* @return Doctrine\ORM\EntityManager
 	*/
-	public function getEm() {
+	public function getEntityM() 
+	{
 		return $this->get('doctrine')->getEntityManager();
 	}
+
+	/**
+	* @return Doctrine\ORM\EntityManager
+	*/
+    public function encodePassword($user) 
+    {
+        $factory = $this->getSecurityEF();
+        $encoder = $factory->getEncoder($user);
+        $password = $encoder->encodePassword($user->getPassword(), $user->getSalt());
+        $user->setPassword($password);
+    }
+    
+	/**
+	* @return
+	*/
+	public function getSecurityEF() 
+	{
+		return $this->get('security.encoder_factory');
+	}
+
+    /**
+     *
+     * @return Security\Core\SecurityContext  
+     */
+    public function getSecurityC() {
+        return $this->get('security.context');
+    }
+    
+    /**
+     * Return the connected user instance
+     * 
+     * @return user instance 
+     */
+    public function getUser() {
+        $token = $this->getSecurityC()->getToken();
+
+        if (!$token)
+            throw ExceptionController::forbiden();
+
+        return $token->getUser();
+    }
+    
 }
