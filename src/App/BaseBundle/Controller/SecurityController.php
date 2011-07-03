98b8b05371bd40bef57546191f4bfe8eb5744a7a
lidaa <aa_dil@hotmail.fr>
2011-07-03 22:06:54 +0200

    Add Security

--- src/App/BaseBundle/Controller/SecurityController.php
+++ src/App/BaseBundle/Controller/SecurityController.php
@@ -16,13 +16,13 @@ class SecurityController extends Controller {
 	*/
     public function loginAction() {
         // get the login error if there is one
-        if ($this->get('request')->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $this->get('request')->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        if ($this->getRequest()->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $this->getRequest()->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
         } else {
-            $error = $this->get('request')->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
+            $error = $this->getRequest()->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
         }
         return $this->render('AppBaseBundle:Security:login.html.php', array(
-            'last_username' => $this->get('request')->getSession()->get(SecurityContext::LAST_USERNAME),
+            'last_username' => $this->getRequest()->getSession()->get(SecurityContext::LAST_USERNAME),
             'error' => $error,
         ));
     }
